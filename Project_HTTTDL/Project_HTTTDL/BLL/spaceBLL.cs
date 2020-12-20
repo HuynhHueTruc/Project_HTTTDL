b1b86ff6549c0a6d63314da68dd4bd0a9b079904
Truc Map <huynhhuetruc@gmail.com>
2020-12-20 16:42:14 +0700

    csld

---
+++ Project_HTTTDL/Project_HTTTDL/BLL/spaceBLL.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json.Linq;
+using Project_HTTTDL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_HTTTDL.BLL
+{
+    public class spaceBLL
+    {
+        public QL_TauXeContext cn;
+        //Ket noi csdl
+        public spaceBLL()
+        {
+            cn = new QL_TauXeContext();
+        }
+
+        //Them du lieu
+        public void AddSpace(JObject data)
+        {
+            
+        }
+
+    }
+}
